0d2f62f8abea77b2f60fa4fe5790bd6d7d30a3fa
a1a2a3a4a5 <linub7@gmail.com>
2022-06-05 15:47:26 +0430

    posts, new post pages, switch to yarn in order to use rich-markdown-editor

--- pages/admin/posts/index.js
+++ pages/admin/posts/index.js
@@ -1,7 +1,27 @@
+import { PlusOutlined } from '@ant-design/icons';
+import { Button, Col, Row } from 'antd';
 import AdminLayout from 'components/admin/layout/AdminLayout';
+import { useRouter } from 'next/router';
 
 const Posts = () => {
-  return <AdminLayout>Posts</AdminLayout>;
+  const router = useRouter();
+  return (
+    <AdminLayout>
+      <Row>
+        <Col span={22} offset={1}>
+          <Button
+            type="primary"
+            style={{ margin: '10px 0px 10px 0px' }}
+            onClick={() => router.push('/admin/posts/new')}
+          >
+            <PlusOutlined />
+            Add New
+          </Button>
+          <h1>Posts</h1>
+        </Col>
+      </Row>
+    </AdminLayout>
+  );
 };
 
 export default Posts;
--- pages/admin/posts/new.js
+++ pages/admin/posts/new.js
@@ -1,7 +1,130 @@
+import { useContext, useState, useEffect } from 'react';
 import AdminLayout from 'components/admin/layout/AdminLayout';
+import { ThemeContext } from 'context/theme';
+import Editor from 'rich-markdown-editor';
+import { Col, Input, Row, Select } from 'antd';
+import axios from 'axios';
+import Resizer from 'react-image-file-resizer';
+
+const { Option } = Select;
 
 const NewPosts = () => {
-  return <AdminLayout>NewPosts</AdminLayout>;
+  const savedTitle = () => {
+    if (typeof window !== 'undefined') {
+      if (localStorage.getItem('post-title')) {
+        return JSON.parse(localStorage.getItem('post-title'));
+      }
+    }
+  };
+
+  const savedContent = () => {
+    if (typeof window !== 'undefined') {
+      if (localStorage.getItem('post-content')) {
+        return JSON.parse(localStorage.getItem('post-content'));
+      }
+    }
+  };
+  const [content, setContent] = useState(savedContent());
+  const [title, setTitle] = useState(savedTitle());
+  const [loadedCategories, setLoadedCategories] = useState([]);
+  const [categories, setCategories] = useState([]);
+
+  const { theme } = useContext(ThemeContext);
+  const handleInputChange = (e) => {
+    setTitle(e.target.value);
+    localStorage?.setItem('post-title', JSON.stringify(e.target.value));
+  };
+
+  const handleContentChange = (value) => {
+    setContent(value());
+    localStorage.setItem('post-content', JSON.stringify(value()));
+  };
+
+  useEffect(() => {
+    getCategories();
+
+    return () => {
+      setLoadedCategories([]);
+    };
+  }, []);
+
+  const getCategories = async () => {
+    try {
+      const { data } = await axios.get('/categories');
+      setLoadedCategories(data?.categories);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleResizeFile = (file) => {
+    return new Promise((resolve, reject) => {
+      Resizer.imageFileResizer(
+        file,
+        720,
+        400,
+        'JPEG',
+        100,
+        0,
+        (uri) => {
+          resolve(uri);
+        },
+        'base64'
+      );
+    });
+  };
+
+  const handleUploadImage = async (file) => {
+    console.log(file);
+    try {
+      const image = await handleResizeFile(file);
+      console.log('Image Base 64: ', image);
+      return;
+      const { data } = await axios.post('/upload-image', { image });
+      console.log('upload file response', data);
+      return data?.url;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <AdminLayout>
+      <Row>
+        <Col xs={22} sm={22} lg={14} offset={1}>
+          <h1>Create new post</h1>
+          <Input
+            placeholder="Give your post a title"
+            size="large"
+            value={title}
+            onChange={handleInputChange}
+          />
+
+          <div className="line"></div>
+          <Editor
+            defaultValue={content}
+            dark={theme === 'dark' ? true : false}
+            onChange={handleContentChange}
+            uploadImage={handleUploadImage}
+          />
+        </Col>
+        <Col xs={22} sm={22} lg={8} offset={1}>
+          <h4>Categories</h4>
+          <Select
+            mode="multiple"
+            allowClear={true}
+            placeholder="Select Categories"
+            style={{ width: '100%' }}
+            onChange={(value) => setCategories(value)}
+          >
+            {loadedCategories?.map((category) => (
+              <Option key={category.name}>{category.name}</Option>
+            ))}
+          </Select>
+        </Col>
+      </Row>
+    </AdminLayout>
+  );
 };
 
 export default NewPosts;
